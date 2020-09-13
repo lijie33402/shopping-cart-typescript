87afff1cdc91b122480f6534c5fcc43a7ace1187
lijie33402 <823997187@qq.com>
2020-09-13 22:57:16 +0800

    改造工程为ts，vuex可以带上ts提示

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
+import ShoppingCart from '@/views/ShoppingCart.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
@@ -17,11 +18,16 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/vuex',
+    name: 'vuex',
+    component: ShoppingCart
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
